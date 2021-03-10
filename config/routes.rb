553ee0ff9c33ef2a2615cb18cefe9316be384b16
Jaden7856 <jhg7856@gmail.com>
2021-03-10 09:58:12 +0900

    AWS Deploy를 위한 파일

--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  get 'hello_page/hello'
-  root 'hello_page#hello'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
