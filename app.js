3ae48ed0617ad9d4ba86adf744a57cf32b1fd980
Jaden Jo <57978668+jaden7856@users.noreply.github.com>
2021-03-10 11:06:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const ip = require('ip');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CodeDeploy로 배포된 AWS exercise의 첫 번째 A project 버전입니다.');
+  res.send('CodeDeploy로 배포된 AWS exercise의 첫 번째 A project 버전입니다.' + ip.address());
 });
 
 app.listen(3000, () => {
